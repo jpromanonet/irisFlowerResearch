3c06894fd744e6f911b23a97d0a3a48d7397d457
jpromanonet <programming@jpromano.net>
2020-02-18 11:24:15 -0300

    Initial commit

---
+++ librariesCheckVersions.py
@@ -0,0 +1,20 @@
+# Check the versions of libraries
+
+# Python version
+import sklearn
+import pandas
+import matplotlib
+import numpy
+import scipy
+import sys
+print('Python: {}'.format(sys.version))
+# scipy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+print('sklearn: {}'.format(sklearn.__version__))
